59dae89ab6105f67d0b320f8d8448dc9859b7ae5
Martin Ondrejka <martin.ondrejka@gmail.com>
2023-12-29 18:52:25 +0100

    Initial commit

---
+++ src/test/java/org/socialnetvaadin2/ApplicationTests.java
@@ -0,0 +1,12 @@
+package org.socialnetvaadin2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    /*@Test
+    void contextLoads() {
+    }*/
+
+}
