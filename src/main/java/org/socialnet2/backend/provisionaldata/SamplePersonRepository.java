116bfc300b58316adad23d0a2b689add4f6dfa9f
Martin Ondrejka <martin.ondrejka@gmail.com>
2024-01-03 08:18:53 +0100

    removed unnecessary classes

--- src/main/java/org/socialnet2/backend/provisionaldata/SamplePersonRepository.java
+++
@@ -1,7 +0,0 @@
-package org.socialnet2.backend.provisionaldata;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface SamplePersonRepository extends JpaRepository<SamplePerson, Long>, JpaSpecificationExecutor<SamplePerson> {
-}
