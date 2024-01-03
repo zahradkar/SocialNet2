25e878bb2e25f2ddd02acd92300e45aa5ecaed15
Martin Ondrejka <martin.ondrejka@gmail.com>
2024-01-03 14:50:22 +0100

    fixed using @Theme annotation

--- src/main/java/org/socialnet2/Application.java
+++ src/main/java/org/socialnet2/Application.java
@@ -1,12 +1,13 @@
 package org.socialnet2;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Theme(value = "default")
-public class Application {
+public class Application implements AppShellConfigurator {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
